119bc64bc29f220e6e4774c16281d9f7ddb485b7
Mikołaj Krawczyk <mkkrawczyk00@gmail.com>
2020-04-28 13:06:20 +0200

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+// npm polifils
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
+// local
+import "../sass/style.scss";
---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+/* eslint-disable */
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: [
+    "core-js/modules/es.promise",
+    "core-js/modules/es.array.iterator",
+    path.resolve(__dirname, "src/js/index.js"),
+  ],
+  devtool: "",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/bundle.js",
+  },
+  devServer: {
+    contentBase: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader",
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|JPE?G)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            outputPath: "images",
+          },
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            // After all CSS loaders we use plugin to do his work.
+            // It gets all transformed CSS and extracts it into separate
+            // single bundled file
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
+            loader: "css-loader",
+          },
+          {
+            // Loader for webpack to process CSS with PostCSS
+            loader: "postcss-loader",
+            options: {
+              plugins() {
+                return [require("autoprefixer")];
+              },
+            },
+          },
+          {
+            // Loads a SASS/SCSS file and compiles it to CSS
+            loader: "sass-loader",
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+    ],
+  },
+};
