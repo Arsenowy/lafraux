963d92f097c3d4862659577e01b9eb54706d8936
Mikołaj Krawczyk <mkkrawczyk00@gmail.com>
2020-05-06 01:51:00 +0200

    Add sketch of collection section

--- src/js/parallax.js
+++ src/js/parallax.js
@@ -3,6 +3,7 @@ const man1 = document.querySelector('.mans__photo--1');
 const man2 = document.querySelector('.mans__photo--2');
 const man3 = document.querySelector('.mans__photo--3');
 let vh = window.innerHeight;
+const section1 = document.querySelector('.collection-1');
 
 //get viewport high when size is changing
 window.addEventListener('resize', () => {
@@ -46,7 +47,7 @@ export const parallaxFunction = () => {
             man2.style.transform = `translate(0, ${offset - vh}px)`;
             }
         if (offset >= (vh+250)){
-            man2.style.fontSize = `${8 + Math.sqrt(offset - 1238)/100}rem`;
+            man2.style.fontSize = `${8 + Math.sqrt(offset - 1238)/85}rem`;
             man2.style.transform = `translate(${-Math.pow(offset - (vh+250), 2)/150}px, ${offset - vh}px)`;
 
             }
@@ -60,7 +61,27 @@ export const parallaxFunction = () => {
         if (offset >= (vh+250)){
             man3.style.transform = `translate(${-Math.pow(offset - (vh+250), 2)/170}px, ${offset - vh}px)`;
         }
+    });
+    window.addEventListener('scroll', () => {
+        let offset = window.pageYOffset;
 
+        if (vh < 1000) {
+            if (offset < 1530){
+                section1.style.transform = `translate(0, ${offset - vh}px)`;
+            if (offset >= 1530 && offset < 1800){
+                section1.style.transform = `translate(0, ${2*vh-0.9*offset}px)`;
+                }
+            }
+        }
+        /////// TESTING responsivnessgit
+        // if (vh >= 1000) {
+        //     if (offset < 1530){
+        //         section1.style.transform = `translate(0, ${offset - vh}px)`;
+        //     if (offset >= 1530 && offset < 2400){
+        //         section1.style.transform = `translate(0, ${2*vh-0.9*offset}px)`;
+        //         }
+        //     }
+        // }
 
     });
 
